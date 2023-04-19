0a1f267f95c2c5ffe29645eb145d63fcff16eda2
bwanis <bwanis@amazon.com>
2023-04-19 12:53:50 -0400

    Update version to 1.1.3

--- source/lib/stage-four.ts
+++ source/lib/stage-four.ts
@@ -1,15 +1,5 @@
-/*********************************************************************************************************************
- *  Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
- *                                                                                                                    *
- *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
- *  with the License. A copy of the License is located at                                                             *
- *                                                                                                                    *
- *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
- *                                                                                                                    *
- *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
- *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
- *  and limitations under the License.                                                                                *
- *********************************************************************************************************************/
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 /**
  * @author Solution Builders
@@ -146,11 +136,9 @@ export class StageFour extends Construct {
             },
         });
 
-        const copyToDestinationBucketPolicy = new iam.Policy(this, "copyToDestinationBucketPolicy", {
-            statements: [iamSec.IamPermissions.lambdaLogGroup(`${Aws.STACK_NAME}-copyToDestinationBucket`)],
-        });
-
-        copyToDestinationBucketPolicy.attachToRole(copyToDestinationBucketRole);
+        copyToDestinationBucketRole.addToPrincipalPolicy(
+            iamSec.IamPermissions.lambdaLogGroup(`${Aws.STACK_NAME}-copyToDestinationBucket`)
+        );
         props.stagingBucket.grantReadWrite(copyToDestinationBucketRole);
         props.statusTable.grantReadWriteData(copyToDestinationBucketRole);
         destinationBucket.grantReadWrite(copyToDestinationBucketRole);
